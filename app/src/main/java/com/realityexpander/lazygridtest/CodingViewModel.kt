5c36f91de4ceb094e59cbdad6b79b3805d75f7ad
Chris Athanas <realityexpander@gmail.com>
2022-09-26 23:51:07 -0500

    Add AnimatedVerticalGrid.ktd

--- app/src/main/java/com/realityexpander/lazygridtest/CodingViewModel.kt
+++ app/src/main/java/com/realityexpander/lazygridtest/CodingViewModel.kt
@@ -1,7 +1,5 @@
 package com.realityexpander.lazygridtest
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
@@ -9,7 +7,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import java.util.Collections.copy
+import java.util.*
+import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.math.abs
 import kotlin.random.Random
 
@@ -23,14 +22,15 @@ class CodingViewModel(
 
     init {
         viewModelScope.launch {
-             codingRepository.getCoding().collect { listCoding ->
-//                 _uiState.update {
-//                     Response.Success(listCoding.sortedBy { it.share })
-//                 }
+            codingRepository.getCoding().collect { listCoding ->
 
-                 _uiState.value = Response.Success(
-                     listCoding.sortedBy { it.share }.reversed()
-                 )
+//                 _uiState.value = Response.Success(
+//                     listCoding.sortedBy { it.share }.reversed()
+//                 )
+
+                _uiState.value = Response.Success(
+                    listCoding
+                )
             }
         }
     }
@@ -62,13 +62,39 @@ class CodingRepository {
 
     fun getCoding(): Flow<List<Coding>> = snapshotFlow {
 
-//        // prevents ConcurrentModificationException (copy trick)
+        // prevents ConcurrentModificationException (copy trick)
+//        val copy = mutableListOf<Coding>()
+//        data.forEach {
+//            copy.add(it.copy())
+//        }
+
 //        val copy = data.map {
 //            it.copy()
 //        }
 //        copy.toList()
 
-        data.toList()
+//        synchronized(data) {
+//            data.map {
+//                it.copy()
+//            }.toList()
+//        }
+
+//        var list: ArrayList<Coding>
+//        synchronized(data) {
+//            list = ArrayList(data.toList())
+//        }
+//        list
+
+        // Super fast
+        val data2 = CopyOnWriteArrayList<Coding>()
+        synchronized(data) {
+            data.forEach(data2::add)
+        }
+        data2
+
+//        synchronized(data) {
+//            data.toList()
+//        }
     }
 
     fun addCoding(coding: Coding): Boolean {
@@ -76,13 +102,13 @@ class CodingRepository {
         return true
     }
 
-    fun removeCoding(coding: Coding):Boolean {
+    fun removeCoding(coding: Coding): Boolean {
         data.remove(coding)
         return true
     }
 
     suspend fun startSimulation() = withContext(Dispatchers.IO) {
-        if(isSimRunning) return@withContext
+        if (isSimRunning) return@withContext
 
         isSimRunning = true
         repeat(1000) {
@@ -115,14 +141,18 @@ class CodingRepository {
 //            data.add(data.removeAt(0)) // forces update (best) (no copy trick)
 
 
-            for(index in data.indices) {
-//                // Causes ConcurrentModificationException without copy trick
-//                data[index] = with(data[index]) {
-//                    copy(
-//                        share = share + trend,
-//                        trend = trend + Random.nextInt(0, 10)
-//                    )
-//                }
+            synchronized(data) {
+                for (index in data.indices) {
+                    data[index] = with(data[index]) {
+                        val randomInt = abs(Random.nextInt()) % 10
+                        copy(
+                            share = share + trend,
+                            trend = trend + randomInt
+                        )
+                    }
+
+                }
+            }
 
 //                // Causes ConcurrentModificationException without copy trick
 //                data[index] = data[index].copy(
@@ -136,7 +166,7 @@ class CodingRepository {
 
 //                // This causes CME (without copy trick)
 //                //data.add(data.removeAt(index)) // forces update on every item changed (needs copy trick)
-            }
+//            }
 //            data.add(data.removeAt(0)) // forces update (needs copy trick)
 
 //            // Causes ConcurrentModificationException without copy trick
@@ -157,13 +187,14 @@ class CodingRepository {
 //                ))
 //            }
 
-            delay(10)
+
+            delay(50)
 
             println("data: ${data
                 .sortedBy { it.share }
                 .reversed()
                 .subList(0,10)
-                .joinToString { it.name + "->" + it.share } 
+                .joinToString { it.name + "->" + it.share }
             }")
         }
 
@@ -172,12 +203,13 @@ class CodingRepository {
 }
 
 
-data class Coding (
+data class Coding(
     val id: Int = 0,
     val name: String = "",
     val description: String = "",
-    val share: Int,
-    val trend: Int
+    val share: Int = 0,
+    val trend: Int = 0,
+    val color: Long = Random(id).nextLong() //and 0xBB000000
 )
 
 enum class CodingType(
@@ -189,26 +221,106 @@ enum class CodingType(
     IOS(2, "iOS", "iOS is a mobile operating system created and developed by Apple Inc."),
     WEB(3, "Web", "Web development is the work involved in developing a Web site for the Internet"),
     OTHER(4, "Other", "Other"),
-    JAVA(5, "Java", "Java is a general-purpose computer-programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible."),
-    KOTLIN(6, "Kotlin", "Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference."),
-    SWIFT(7, "Swift", "Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc."),
-    JAVASCRIPT(8, "JavaScript", "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification."),
-    PYTHON(9, "Python", "Python is an interpreted, high-level, general-purpose programming language."),
-    RUBY(10, "Ruby", "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language."),
-    C_SHARP(11, "C#", "C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines."),
-    C_PLUS_PLUS(12, "C++", "C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or \"C with Classes\"."),
-    C(13, "C", "C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system."),
-    GO(14, "Go", "Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson."),
-    RUST(15, "Rust", "Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency."),
-    PHP(16, "PHP", "PHP is a general-purpose scripting language especially suited to web development."),
-    SCALA(17, "Scala", "Scala is a general-purpose programming language providing support for functional programming and a strong static type system."),
-    R(18, "R", "R is a programming language and software environment for statistical computing and graphics supported by the R Foundation for Statistical Computing."),
-    DART(19, "Dart", "Dart is a client-optimized programming language for apps on multiple platforms."),
-    OBJECTIVE_C(20, "Objective-C", "Objective-C is a general-purpose, object-oriented programming language that adds Smalltalk-style messaging to the C programming language."),
-    PERL(21, "Perl", "Perl is a family of high-level, general-purpose, interpreted, dynamic programming languages."),
-    HASKELL(22, "Haskell", "Haskell is a standardized, general-purpose purely functional programming language, with non-strict semantics and strong static typing."),
-    CLOJURE(23, "Clojure", "Clojure is a dialect of the Lisp programming language, with a focus on functional programming."),
-    COBOL(24, "COBOL", "COBOL is a compiled English-like computer programming language designed for business use."),
+    JAVA(
+        5,
+        "Java",
+        "Java is a general-purpose computer-programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible."
+    ),
+    KOTLIN(
+        6,
+        "Kotlin",
+        "Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference."
+    ),
+    SWIFT(
+        7,
+        "Swift",
+        "Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc."
+    ),
+    JAVASCRIPT(
+        8,
+        "JavaScript",
+        "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification."
+    ),
+    PYTHON(
+        9,
+        "Python",
+        "Python is an interpreted, high-level, general-purpose programming language."
+    ),
+    RUBY(
+        10,
+        "Ruby",
+        "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language."
+    ),
+    C_SHARP(
+        11,
+        "C#",
+        "C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines."
+    ),
+    C_PLUS_PLUS(
+        12,
+        "C++",
+        "C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or \"C with Classes\"."
+    ),
+    C(
+        13,
+        "C",
+        "C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system."
+    ),
+    GO(
+        14,
+        "Go",
+        "Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson."
+    ),
+    RUST(
+        15,
+        "Rust",
+        "Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency."
+    ),
+    PHP(
+        16,
+        "PHP",
+        "PHP is a general-purpose scripting language especially suited to web development."
+    ),
+    SCALA(
+        17,
+        "Scala",
+        "Scala is a general-purpose programming language providing support for functional programming and a strong static type system."
+    ),
+    R(
+        18,
+        "R",
+        "R is a programming language and software environment for statistical computing and graphics supported by the R Foundation for Statistical Computing."
+    ),
+    DART(
+        19,
+        "Dart",
+        "Dart is a client-optimized programming language for apps on multiple platforms."
+    ),
+    OBJECTIVE_C(
+        20,
+        "Objective-C",
+        "Objective-C is a general-purpose, object-oriented programming language that adds Smalltalk-style messaging to the C programming language."
+    ),
+    PERL(
+        21,
+        "Perl",
+        "Perl is a family of high-level, general-purpose, interpreted, dynamic programming languages."
+    ),
+    HASKELL(
+        22,
+        "Haskell",
+        "Haskell is a standardized, general-purpose purely functional programming language, with non-strict semantics and strong static typing."
+    ),
+    CLOJURE(
+        23,
+        "Clojure",
+        "Clojure is a dialect of the Lisp programming language, with a focus on functional programming."
+    ),
+    COBOL(
+        24,
+        "COBOL",
+        "COBOL is a compiled English-like computer programming language designed for business use."
+    ),
 
 }
 
--- app/src/main/java/com/realityexpander/lazygridtest/MainActivity.kt
+++ app/src/main/java/com/realityexpander/lazygridtest/MainActivity.kt
@@ -4,23 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColor
+import androidx.core.graphics.toColorLong
 import com.realityexpander.lazygridtest.ui.theme.LazyGridTestTheme
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,8 +37,6 @@ class MainActivity : ComponentActivity() {
 
                 val scope = rememberCoroutineScope()
 
-//                val listState = rememberLazyListState()
-//
 //                LaunchedEffect(uiState.value.data) {
 //                    snapshotFlow { uiState.value.data}
 //                        .distinctUntilChanged()
@@ -41,41 +45,100 @@ class MainActivity : ComponentActivity() {
 //                        }
 //                }
 
+                val itemsSorted = uiState.value.data
+                    ?.map { it.copy() }
+                    ?.sortedBy {
+                        it.share
+                    }
+                    ?.reversed()
+                    ?: emptyList()
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LazyColumn {
-                        items(uiState.value.data ?: emptyList()) { item ->
-                            Text(text = item.name +":"+ item.share)
+//                    LazyVerticalGrid(
+//                        cells = GridCells.Fixed(2),
+//                        state = listState,
+//                    ) {
+//                        items(itemsSorted) { item ->
+//                            Row {
+//                                Text(text = item.name +":"+ item.share,
+//                                    modifier = Modifier.animateItemPlacement())
+//                            }
+//                        }
+
+//                    LazyColumn() {
+//                        items(
+//                            //itemsSorted,
+//                            uiState.value.data ?: emptyList(),
+//                            key= { item -> item.id },
+//                        ) { item ->
+//                            Text(text = item.name +":"+ item.share,
+//                                modifier = Modifier.animateItemPlacement()
+//                            )
+//                        }
+
+
+//                        item {
+//                            Button(onClick = {
+//                                scope.launch {
+//                                    viewModel.startSim()
+//                                }
+//                            }) {
+//                                Text(text = "Start Sim")
+//                            }
+//                        }
+//                    }
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White)
+                    ) {
+                        AnimatedVerticalGrid(
+                            items = itemsSorted,
+                            itemKey = Coding::id,
+                            columns = 2,
+                            rows = itemsSorted.size / 2,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1F)
+                        ) { item ->
+                            //Coding(it.id)
+                            Text(
+                                text = item.name + ":" + item.share,
+                                color = Color.Black,
+                                modifier = Modifier.
+                                    background(color = Color(item.color))
+                                    .padding(8.dp)
+                            )
                         }
 
-                        item {
-                            Button(onClick = {
-                                scope.launch {
-                                    viewModel.startSim()
-                                }
-                            }) {
-                                Text(text = "Start Sim")
+                        Button(onClick = {
+                            scope.launch {
+                                viewModel.startSim()
                             }
+                        }) {
+                            Text(text = "Start Sim")
                         }
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LazyGridTestTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        LazyGridTestTheme {
+            Greeting("Android")
+        }
     }
 }
